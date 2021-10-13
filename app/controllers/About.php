fa27f7cf6933a5e5b1414ef6bd92486897c4e508
daffaaryapp <daffaaryapp@gmail.com>
2021-10-13 20:18:37 +0700

    Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php 
+
+class About {
+    public function Index($nama = "Daffa" , $pekerjaan = "IT"){
+        echo "Nama saya $nama, saya adalah seorang $pekerjaan" ;
+    }
+    public function page(){
+        echo "About/page";
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,10 +1,41 @@
 <?php 
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'Index';
+    protected $params = [];
+
     public function __construct()
     {
         $url = $this->parseURL();
-        var_dump($url);  
+        
+        //controller
+        if(isset($url[0])){
+            if(file_exists('../app/controllers/'. $url[0] . '.php')){
+                $this->controller = $url[0];
+                unset($url[0]);
+            }
+        }
+        //mengikutsertakan controller
+        require_once '../app/controllers/'. $this->controller .'.php';
+        //di instance agar dapat memasukkan method
+        $this->controller = new $this->controller;
+
+        //method 
+        if(isset($url[1])){
+            if(method_exists($this->controller , $url[1])){
+            $this->method = $url[1];
+            unset($url[1]);
+            }
+        }
+        //params
+        if(!empty($url)){
+            $this->params = array_values($url);
+            
+        }
+
+        //jalankan controller dan method, serta kirimkan params jika ada
+        call_user_func_array([$this->controller,$this->method],$this->params); 
     }
 
     //memecah url menjadi sebuah array
