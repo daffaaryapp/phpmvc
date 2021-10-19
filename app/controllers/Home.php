8104af51825e7652837382cafe57c4a53f4c9a51
daffaaryapp <daffaaryapp@gmail.com>
2021-10-19 23:05:29 +0700

    menambahkan sedikit komentar dan image

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,7 +3,9 @@
 class Home extends Controller {
     public function index(){
         $data['judul'] = 'Home Index';
-        $data['nama'] = $this->model('User_model')->getUser();;
+        $data['nama'] = $this->model('User_model')->getUser();
+
+        //tampilkan halaman
         $this->view('templates/header',$data);
         $this->view('home/index',$data);
         $this->view('templates/footer');
